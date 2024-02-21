147c8e33be5d41ad046425b7fa31b972caae9477
Dan B Dev <danbuckleitner@gmail.com>
2024-02-21 10:17:07 +0200

    debug connections creation

--- main.go
+++ main.go
@@ -26,13 +26,16 @@ type RailMap struct {
 
 // displays program usage
 func displayHelp() {
-	fmt.Printf("train scheduler usage:\ngo run . [path to file containing network map] [start station] [end station] [number of trains]")
+	fmt.Printf("train scheduler usage:\ngo run . [path to file containing network map] [start station] [end station] [number of trains]\n")
 }
 
-// builds stations array
+// builds stations array and connections graph
 func buildStations(filePath string) ([]Station, RailMap) {
 	var stations []Station
-	var connections RailMap
+	connections := RailMap{
+		Stations:    make([]*Station, 0),
+		Connections: make(map[*Station][]*Station),
+	}
 	mapFile, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println("Error:", err)
@@ -40,8 +43,8 @@ func buildStations(filePath string) ([]Station, RailMap) {
 	}
 	defer mapFile.Close()
 	scanner := bufio.NewScanner(mapFile)
+	place := true
 	for scanner.Scan() {
-		place := true
 		//build stations until "connections:" is hit
 		line := scanner.Text()
 		if line == "stations:" {
@@ -98,7 +101,8 @@ func addConnection(line string, stations []Station, connections RailMap) RailMap
 		//optionally quit with error
 		return connections
 	}
-	//possibly check here for redundant or reverse connections
+	// NOTE possibly check here for redundant or reverse connections
+
 	connections.Connections[stop1] = append(connections.Connections[stop1], stop2)
 	connections.Connections[stop2] = append(connections.Connections[stop2], stop1)
 	return connections
@@ -106,9 +110,9 @@ func addConnection(line string, stations []Station, connections RailMap) RailMap
 
 // looks up station by name string, returns pointer or nil if not found
 func stationLookup(name string, stations []Station) *Station {
-	for _, station := range stations {
-		if station.Name == name {
-			return &station
+	for i := range stations {
+		if stations[i].Name == name {
+			return &stations[i] // Return the address of the found station
 		}
 	}
 	return nil
@@ -133,4 +137,14 @@ func main() {
 	//build our slice of stations and the map
 	stations, connections := buildStations(networkMap)
 
+	fmt.Printf("\nstart: %v  end: %v  numTrains: %v\n", start, end, numTrains)
+	fmt.Println("stations: ")
+	fmt.Println(stations)
+	fmt.Println("connections:")
+	for station, lines := range connections.Connections {
+		fmt.Println(*&station.Name)
+		for _, stops := range lines {
+			fmt.Println("\t", *&stops.Name)
+		}
+	}
 }
